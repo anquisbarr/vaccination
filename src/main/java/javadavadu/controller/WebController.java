a65b2aa4caa9d926b68a8ab48d902a9358716206
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-05-17 12:53:11 -0500

    ready

--- src/main/java/javadavadu/controller/WebController.java
+++ src/main/java/javadavadu/controller/WebController.java
@@ -3,14 +3,12 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-@RestController
-@RequestMapping("/")
+@Controller
 public class WebController {
+
+	@GetMapping("/")
 	public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "index";
