3c9fd7b83d2fe0eabc9c573584fd87b9ed564ca7
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-05-17 12:09:42 -0500

    Fix?

--- src/main/java/javadavadu/controller/WebController.java
+++ src/main/java/javadavadu/controller/WebController.java
@@ -5,10 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class WebController {
-
-	@GetMapping("/")
 	public String index(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
 		return "index";
