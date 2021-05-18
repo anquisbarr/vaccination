441d1190274e10ff95463d01ceadabb594c9ffe2
anquisbarr <sebastian.quispe.b@utec.edu.pe>
2021-05-18 00:09:21 -0500

    aber.2

--- src/main/java/javadavadu/controller/WebController.java
+++ src/main/java/javadavadu/controller/WebController.java
@@ -2,8 +2,7 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
